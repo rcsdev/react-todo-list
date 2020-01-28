a8fff6736809670fd7cce3c65fdf52858d86a55c
Ruben C <rcastro@mysphera.com>
2020-01-28 11:44:06 +0100

    Added project files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../redux/containers/Header'
+import MainSection from '../redux/containers/MainSection'
+
+const App = () => (
+  <div>
+    <Header />
+    <MainSection />
+  </div>
+)
+
+export default App
